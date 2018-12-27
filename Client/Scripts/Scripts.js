11b4573e564a9ef793b7e279832c9ef13ac7012c
Dmitry <marutkin.dmitriy@gmail.com>
2018-12-27 16:47:41 +0300

    'Initial'

---
+++ Client/Scripts/Scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // const addEventListener = document.addEventListener;
+    // const projectsElement = document.getElementById('projects');
+    // let lastScrollTop = 0;
+    // let scrollCache = 0;
+    // addEventListener("scroll", function () {
+    //     let st = window.pageYOffset || document.documentElement.scrollTop;
+    //     if (st > lastScrollTop) {
+    //         /* scroll down */
+    //         scrollCache = (scrollCache + 15) > 550 ? (scrollCache + 15) : 550;
+    //     } else {
+    //         /* scroll up */
+    //         scrollCache = (scrollCache - 25) > 0 ? (scrollCache - 25) : 0;
+    //     }
+    //     projectsElement.style.bottom = scrollCache + 'px';
+    //     console.log(st, scrollCache);
+    //     lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+    // }, false);
+});
