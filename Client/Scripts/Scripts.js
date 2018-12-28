cc94fa4d283d5c170ae75a213e1b2bd58bb21d75
Dmitry <marutkin.dmitriy@gmail.com>
2018-12-28 14:17:54 +0300

    Added skills section

--- Client/Scripts/Scripts.js
+++ Client/Scripts/Scripts.js
@@ -1,19 +1,36 @@
+function fadeIn(element, time) {
+    let last = +new Date();
+    let tick = function() {
+        element.style.opacity = String(Number(element.style.opacity) + (new Date() - last) / time);
+        last = +new Date();
+
+        if (Number(element.style.opacity) < 1) {
+            (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+        }
+    };
+    tick();
+}
+function hide(element){
+    element.style.opacity = '0';
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-    // const addEventListener = document.addEventListener;
-    // const projectsElement = document.getElementById('projects');
-    // let lastScrollTop = 0;
-    // let scrollCache = 0;
-    // addEventListener("scroll", function () {
-    //     let st = window.pageYOffset || document.documentElement.scrollTop;
-    //     if (st > lastScrollTop) {
-    //         /* scroll down */
-    //         scrollCache = (scrollCache + 15) > 550 ? (scrollCache + 15) : 550;
-    //     } else {
-    //         /* scroll up */
-    //         scrollCache = (scrollCache - 25) > 0 ? (scrollCache - 25) : 0;
-    //     }
-    //     projectsElement.style.bottom = scrollCache + 'px';
-    //     console.log(st, scrollCache);
-    //     lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-    // }, false);
+    // Elements
+    let skillsElement = document.getElementById("skills");
+    hide(skillsElement);
+    // Cache
+    let lastScrollTop = 0;
+    let scrollCache = 0;
+    document.addEventListener("scroll", function () {
+        let st = window.pageYOffset || document.documentElement.scrollTop;
+        if (st > lastScrollTop) {
+            /* scroll down */
+            scrollCache = (scrollCache + 15) > 550 ? (scrollCache + 15) : 550;
+            fadeIn(skillsElement, 2000);
+        } else {
+            /* scroll up */
+            scrollCache = (scrollCache - 25) > 0 ? (scrollCache - 25) : 0;
+        }
+        lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+    }, false);
 });
